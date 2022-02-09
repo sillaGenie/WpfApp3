b75787e07e71ce687caedf94780555223651b924
Татьяна Буянова <tanyasilagina@gmail.com>
2022-02-09 14:41:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.TextDecorations == null)
+                {
+                    textBox.TextDecorations = TextDecorations.Underline;
+                }
+                else 
+                    textBox.TextDecorations = null;
+            }
+        }
+
+        
+        private void Image_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontWeight != FontWeights.Bold)
+                {
+                    textBox.FontWeight = FontWeights.Bold;
+                }
+                else
+                    textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void Image_MouseLeftButtonDown_2(object sender, MouseButtonEventArgs e)
+        {
+
+            if (textBox != null)
+            {
+                if (textBox.FontStyle != FontStyles.Italic)
+                {
+                    textBox.FontStyle = FontStyles.Italic;
+                }
+                else
+                    textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                
+                    textBox.Foreground = Brushes.Black;
+               
+            }
+        }
+    }
+}
